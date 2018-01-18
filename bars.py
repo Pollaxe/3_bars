ff92b07363da76a765412d00a91e9dfbda28a72d
Pollaxe <bobrax@yandex.ru>
2018-01-18 16:13:16 +0300

    доделал прогу

--- bars.py
+++ bars.py
@@ -1,21 +1,48 @@
 import json
+import sys
+from math import sqrt
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    json_content = open(file_path,
+                        encoding='utf-8').read()
+    return json_content
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(json_decoded):
+    biggest_bar = max(json_decoded,
+                      key=lambda qty: qty['properties']['Attributes']['SeatsCount'])
+    return biggest_bar
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(json_decoded):
+    smallest_bar = min(json_decoded,
+                       key=lambda qty: qty['properties']['Attributes']['SeatsCount'])
+    return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(json_decoded, latitude, longitude):
+    closest_bar = min(json_decoded,
+                      key=lambda coords: sqrt((coords['geometry']['coordinates'][0] - latitude) ** 2 +
+                                              (coords['geometry']['coordinates'][1] - longitude) ** 2))
+    return closest_bar
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_path = sys.argv[1]
+        json_content = load_data(file_path)
+        json_decoded = json.loads(json_content)
+        json_decoded = json_decoded['features']
+        biggest_bar = get_biggest_bar(json_decoded)
+        smallest_bar = get_smallest_bar(json_decoded)
+        print('Biggest bar in Moscow is ' + biggest_bar['properties']['Attributes']['Name'])
+        print('Smallest bar in Moscow is ' + smallest_bar['properties']['Attributes']['Name'])
+        latitude = float(input('Please input your latitude:'))
+        longitude = float(input('Please input your longitude:'))
+        closest_bar = get_closest_bar(json_decoded, latitude, longitude)
+        print('Closest bar in Moscow to you is ' + closest_bar['properties']['Attributes']['Name'])
+    except IndexError:
+        print('Please use syntax: "python pprint_json.py <filename>"')
+    except FileNotFoundError:
+        print('File not found, please try again.')
