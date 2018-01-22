d1d030e761de85a908ad0f845d91c4e184afe034
Pollaxe <bobrax@yandex.ru>
2018-01-22 16:59:53 +0300

    renamed functions

--- bars.py
+++ bars.py
@@ -4,10 +4,10 @@ from math import sqrt
 
 
 def load_data(file_path):
-    with open(file_path, encoding='utf-8') as content:
-        json_content = content.read()
-        bars = json.loads(json_content)
-    return bars
+    with open(file_path, encoding='utf-8') as file_object:
+        json_content = file_object.read()
+        json_decoded = json.loads(json_content)
+    return json_decoded
 
 
 def get_biggest_bar(bars):
@@ -28,40 +28,40 @@ def get_smallest_bar(bars):
 
 def get_closest_bar(bars, latitude, longitude):
     closest_bar = min(bars,
-                      key=lambda coords:
+                      key=lambda bar:
                       sqrt((
-                        coords['geometry']['coordinates'][0] - latitude) ** 2 +
-                        (coords['geometry']['coordinates'][1] - longitude) ** 2
-                           ))
+                        bar['geometry']['coordinates'][0] - latitude) ** 2 +
+                        (bar['geometry']['coordinates'][1] - longitude) ** 2
+                        ))
     return closest_bar
 
 
 if __name__ == '__main__':
     try:
         file_path = sys.argv[1]
-        bars = load_data(file_path)
+        json_decoded = load_data(file_path)
     except FileNotFoundError:
-        print('Файл не найден, попробуйте еще раз.')
-        sys.exit()
+        sys.exit('Файл не найден, попробуйте еще раз.')
     except IndexError:
-        print('Используйте синтакс: "python bars.py <filename>"')
-        sys.exit()
-    bars = bars['features']
+        sys.exit('Используйте синтаксис: "python bars.py <filename>"')
+    bars = json_decoded['features']
     biggest_bar = get_biggest_bar(bars)
     smallest_bar = get_smallest_bar(bars)
-    print('{} {}'.format('Самый большой бар в Москве:',
-                         biggest_bar['properties']['Attributes']['Name']
-                         ))
-    print('{} {}'.format('Самый маленький бар в Москве:',
-                         smallest_bar['properties']['Attributes']['Name']
-                         ))
+    print('{} {}'.format
+          ('Самый большой бар в Москве:',
+           biggest_bar['properties']['Attributes']['Name']
+           ))
+    print('{} {}'.format
+          ('Самый маленький бар в Москве:',
+           smallest_bar['properties']['Attributes']['Name']
+           ))
     try:
         latitude = float(input('Введите вашу широту:'))
         longitude = float(input('Введите вашу долготу:'))
         closest_bar = get_closest_bar(bars, latitude, longitude)
-        print('{} {}'.format('Самый ближайший к вам бар:',
-                             closest_bar['properties']['Attributes']['Name']
-                             ))
+        print('{} {}'.format
+              ('Самый ближайший к вам бар:',
+               closest_bar['properties']['Attributes']['Name']
+               ))
     except ValueError:
-        print('Не были введены координаты.')
-        sys.exit()
+        sys.exit('Не были введены координаты.')
